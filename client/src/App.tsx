5e08483c8d6c20fdab44ec2c3ba8911e36e2b4d2
WilerMS <wilersoriano@gmail.com>
2023-06-24 05:09:25 +0200

    env variables included

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,10 @@ import Loading from '@components/Loading'
 import cn from 'classnames'
 import SuccesUploadedImage from '@components/SuccesUploadedImage'
 import { useFiles } from '@hooks/useFiles'
+import { API_URL } from './constants'
 
 function App () {
-  const { data, isLoading, postFile } = useFiles('http://localhost:4000/files')
+  const { data, isLoading, postFile } = useFiles(`${API_URL}/files`)
   const handleUploadFile = (file: File) => { postFile(file) }
 
   const height = !isLoading && data
@@ -18,7 +19,7 @@ function App () {
     <div className='App'>
       <div className={cn(height, 'w-[402px] bg-white rounded-[12px] p-[32px] shadow-[0px_4px_12px_0px_#0000001A] transition-all ease-out duration-200')}>
         {!isLoading && data
-          ? <SuccesUploadedImage file={`http://localhost:4000${data}`} />
+          ? <SuccesUploadedImage file={`${API_URL}${data}`} />
           : isLoading
             ? <Loading />
             : <FileUploader onUploadFile={handleUploadFile} />
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,18 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
 import router from './routes'
+import { PORT } from './constants'
 
 const app = express()
 
 // Middlewares
 app.use(cors())
-app.use(express.json())
 app.use(logger('tiny'))
 app.use(express.static('files'))
 
 app.use(router)
-
-const PORT = 4000
 
 app.listen(PORT)
 console.log('Server on port ' + PORT)
